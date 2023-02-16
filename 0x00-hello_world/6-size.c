e3864456ce71c19cb62b3f2703fbc3bd251d308d
louzanizineddine <louzanizineddine@gmail.com>
2023-02-16 09:01:09 -0800

    variable size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints sized of characters
  *
- * Return: Always 0 (sucess)
+ *Return: Always 0 (success)
  */
-
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(double));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-printf("Size of a char: %ld byte(s)\n", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
