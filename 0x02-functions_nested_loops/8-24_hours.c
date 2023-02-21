e79bc7cfd63b60aa4b7359f2fec35d281b284f7b
louzanizineddine <louzanizineddine@gmail.com>
2023-02-21 13:43:03 -0800

    task 9, 10

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h_tens, h_ones, m_tens, m_ones, h_max;
+
+	h_max = 58;
+	h_tens = '0';
+	while (h_tens < '3')
+	{
+		if (h_tens == '2')
+		{
+			h_max = '4';
+		}
+		h_ones = '0';
+		while (h_ones < h_max)
+		{
+			m_tens = '0';
+			while (m_tens < '6')
+			{
+				m_ones = '0';
+				while (m_ones < 58)
+				{
+					_putchar(h_tens);
+					_putchar(h_ones);
+					_putchar(':');
+					_putchar(m_tens);
+					_putchar(m_ones);
+					_putchar('\n');
+					m_ones++;
+				}
+				m_ones = '0';
+				m_tens++;
+			}
+			m_tens = '0';
+			h_ones++;
+		}
+		h_ones = '0';
+		h_tens++;
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int r, c, p, tens, ones;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			p = r * c;
+			tens = p / 10;
+			ones = p % 10;
+
+			if (c == 0)
+			{
+				_putchar('0');
+			}
+			else if (p < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
