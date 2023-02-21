45049a91e08e99438efc2cea53d5cc345efac51a
louzanizineddine <louzanizineddine@gmail.com>
2023-02-21 13:16:43 -0800

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int j = 0;
+
+	while (j <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		j++;
+	}
+}
