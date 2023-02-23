5f19555af46eba8e4872ec7d74967223ea468efd
louzanizineddine <louzanizineddine@gmail.com>
2023-02-23 15:26:12 -0800

    task 8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz code
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int n= 1;
+
+	while (n<= 100)
+	{
+		if (n% 3 == 0 && n% 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n% 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n% 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", n);
+		}
+		if (n!= 100)
+		{
+			putchar(' ');
+		}
+
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
