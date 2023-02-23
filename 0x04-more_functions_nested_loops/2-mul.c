dfb6b8083c4f1fb3c137002b5b119b4fd81e2ba0
louzanizineddine <louzanizineddine@gmail.com>
2023-02-23 14:50:57 -0800

    task 1,2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - returns the mult of a and b
+ * @a: integer variable
+ * @b: integer variable
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
