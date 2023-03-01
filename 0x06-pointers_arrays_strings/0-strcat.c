2e7b6e1b751567722112aad4f025cfd2c0429a49
louzanizineddine <louzanizineddine@gmail.com>
2023-03-01 08:44:17 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,14 @@
  * @dest: destiation
  * Return:pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-while (dest[i] != '\0')
-i++;
-int j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-return (dest);
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+	return (dest);
 }
