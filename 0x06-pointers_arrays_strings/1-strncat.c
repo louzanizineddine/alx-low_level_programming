a0aa5a440a60ff9b8b13defd7a27b689af973c25
louzanizineddine <louzanizineddine@gmail.com>
2023-03-01 10:01:44 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- **_strcat - concatenates sring src to string dest
+ **_strncat - concatenates sring src to string dest
  * @src: source string
  * @dest: destiation
+ * @n: number of bytes from source to use
  * Return:pointer to the resulting string
  */
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
