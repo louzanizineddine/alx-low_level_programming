896d17f7d5a9b6a72515b351739203bc051a963e
louzanizineddine <louzanizineddine@gmail.com>
2023-03-06 09:52:28 -0800

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: zineddine
- * Desc: Header file that conatains prototypes for functions
- *       used in the 0x07-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
-
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
