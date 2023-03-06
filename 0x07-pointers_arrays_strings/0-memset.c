b7ea4d0957018c1d677610303c03bf238957671b
louzanizineddine <louzanizineddine@gmail.com>
2023-03-06 10:13:52 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,14 @@
  * Return: s
  */
 
-char _memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (; n > 0; i++; n--)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
+n--;
 	}
 
 	return (s);
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
+#include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
-void _memset(void *s, int c, size_t n);
-void _memcpy(void *dest, const void *src, size_t n);
-char _strchr(char *s, char c);
+char *_memset(char *s, char b, unsigned int n);
+void *_memcpy(void *dest, const void *src, size_t n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char _strpbrk(char *s, char *accept);
 char _strstr(char *haystack, char *needle);
