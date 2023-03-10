58f4dc39fffe114a5414e98854eea905ce964ec5
louzanizineddine <louzanizineddine@gmail.com>
2023-03-10 02:26:10 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+   * main - main function entry point
+   * @argc: Numner of command line arguments
+   * @argv: Array of arguments
+   * Return: 0
+*/
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
