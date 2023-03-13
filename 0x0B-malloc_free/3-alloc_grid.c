b279f136446c54856da8077a31f4c2c320eca418
louzanizineddine <louzanizineddine@gmail.com>
2023-03-13 14:17:01 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - concatenates two strings.
+ * @height: height.
+ * @width: width
+ * Return: pointet to 2D array.
+ */
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	for (j = 0; j < width; j++)
+	{
+		grid[i][j] = 0;
+	}
+	}
+	return (grid);
+}
+
