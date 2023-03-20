ad3b12c07a2ea09cb9e08f625277c1e4bbda14af
louzanizineddine <louzanizineddine@gmail.com>
2023-03-20 08:58:16 -0700

    task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes sum of x + y.
+ */
+
+#define SUM(x,y) ((x) + (y))
+
+#endif
