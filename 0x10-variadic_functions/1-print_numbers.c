1c3bd0be73986150ca2dad1ebd9e039fa96d8505
louzanizineddine <louzanizineddine@gmail.com>
2023-03-23 07:03:19 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+  * print_numbers- sum all number in args.
+  *@separator: the seperator to use between numbers.
+  *@n: numbers of args to come.
+  *@...: args to come.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(ap);
+}
