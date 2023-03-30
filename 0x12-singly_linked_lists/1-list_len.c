6e226c746f5f53a6a29e6f97eafaf92aa8b64e98
louzanizineddine <louzanizineddine@gmail.com>
2023-03-30 06:42:33 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,10 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
+ * list_len - Finds the number of nodes in
  * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
  *
- * Return: The number of elements in h.
+ * Return: The number of nodes  in h.
  */
 size_t list_len(const list_t *h)
 {
