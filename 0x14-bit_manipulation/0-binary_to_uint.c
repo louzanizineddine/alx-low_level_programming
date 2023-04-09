7649c014f2b2716e10badf5280ba5beee9bf1de3
louzanizineddine <louzanizineddine@gmail.com>
2023-04-09 15:52:35 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,22 @@
  * Return: unsigned int.
  */
 
-unsigned int binary_to_uint(const char *b) {
-  unsigned int result = 0;
-  
-  if (b == NULL)
-    return 0;
-  
-  for (; *b != '\0'; b++) {
-    if (*b == '0')
-      result = (result << 1) | 0;
-    else if (*b == '1')
-      result = (result << 1) | 1;
-    else
-      return 0;
-  }
-  
-  return result;
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+	for (; *b != '\0'; b++)
+	{
+		if (*b == '0')
+			result = (result << 1) | 0;
+		else if (*b == '1')
+			result = (result << 1) | 1;
+		else
+			return (0);
+	}
+
+	return (result);
 }
 
