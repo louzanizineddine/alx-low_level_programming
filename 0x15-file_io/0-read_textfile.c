5cc20a1b220a0d3360473180fa2fe70ee8e0504f
louzanizineddine <louzanizineddine@gmail.com>
2023-04-10 13:06:34 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,12 +15,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * num_letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
-	num_bytes_read = read(file_descriptor, buffer, num_letters);
+	num_bytes_read = read(file_descriptor, buffer, letters);
 	num_bytes_written = write(STDOUT_FILENO, buffer, num_bytes_read);
 
 	if (file_descriptor == -1 || num_bytes_read == -1 ||
