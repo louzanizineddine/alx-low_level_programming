9e68c278dcccd4ddc9043ed466d926f42ec0df6a
louzanizineddine <louzanizineddine@gmail.com>
2023-04-10 18:24:05 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file.
+ * create_file - create a new file.
  * @filename: A pointer to the name of the file.
  * @text_content: The string to add to the end of the file.
  *
  * Return: number of chars appended.
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int o, written_chars, length = 0;
+	int file, count, length = 0;
 
-	if (filengthame == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
@@ -20,13 +20,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	written_chars = open(filengthame, O_WRONLY | O_APPEND);
-	w = write(written_chars, text_content, length);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	count = write(file, text_content, length);
 
-	if (written_chars == -1 || w == -1)
+	if (file == -1 || count == -1)
 		return (-1);
 
-	close(o);
+	close(file);
 
 	return (1);
 }
